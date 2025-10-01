892ee33ccbf16e6fb092cbf39d60e5e7e15c1c1e
ThengdeNitin <nitinthengde14368@gmail.com>
2025-10-01 14:57:45 +0530

    bug fixed

--- backend/server.js
+++ backend/server.js
@@ -59,7 +59,7 @@ const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    await connectDB(); // await DB connection
+    await connectDB(); 
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   } catch (err) {
     console.error("Failed to connect to MongoDB:", err);
