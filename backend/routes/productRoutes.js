10e7e2f0e61cf98d771114653d7b673c89e963f8
ThengdeNitin <nitinthengde14368@gmail.com>
2025-10-01 15:04:22 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { 
+import {
   uploadProductImage,
   addProduct,
   updateProductDetails,
@@ -19,22 +19,18 @@ import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
+router.get("/", fetchProducts); 
+router.get("/allproducts", fetchAllProducts); 
+router.get("/top", fetchTopProducts); 
+router.get("/new", fetchNewProducts); 
+router.get("/:id", fetchProductById); 
+router.post("/filtered-products", filterProducts); 
+
 router.post("/uploads", upload.single("image"), uploadProductImage);
-
-router.get("/", fetchProducts);
-router.get("/allproducts", fetchAllProducts);
-router.get("/top", fetchTopProducts);
-router.get("/new", fetchNewProducts);
-
 router.post("/", authenticate, authorizeAdmin, addProduct);
-
-router
-  .route("/:id")
-  .get(fetchProductById)
-  .put(authenticate, authorizeAdmin, updateProductDetails)
-  .delete(authenticate, authorizeAdmin, removeProduct);
+router.put("/:id", authenticate, authorizeAdmin, updateProductDetails);
+router.delete("/:id", authenticate, authorizeAdmin, removeProduct);
 
 router.post("/:id/reviews", authenticate, checkId, addProductReview);
-router.post("/filtered-products", filterProducts);
 
 export default router;
